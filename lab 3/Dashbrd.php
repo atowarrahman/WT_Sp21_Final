3a7e35258744a6483933b6412ba0655a5d7be86a
Atowar Rahman <emonatowarrahman@gmail.com.com>
2021-03-28 22:06:23 +0600

    first commit

--- lab 3/Dashbrd.php
+++ lab 3/Dashbrd.php
@@ -2,7 +2,11 @@
 <head>
 </head>
 <body>
-    <a href="Students.php">All Student</a>
-    <a href="AddStudents.php">Add Student</a>
+    <a href="Students.php">All Student</a><br>
+    <a href="AddStudents.php">Add Student</a><br>
+    <a href="AllDepartment.php">ADD Department</a><br>
+	<a href="AddDepartment.php">All Department</a>
+
+
 </body>
 </html>
